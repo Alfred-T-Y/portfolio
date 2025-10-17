1df69b97d3885cf09681c61adbf3f82b7e382c34
Alfred-T-Y <tchoppeyonaalfred@gmail.com>
2025-10-17 22:30:35 +0100

    first commit

---
+++ src/sections/Hero.js
@@ -0,0 +1,9 @@
+export const roles = [
+    {   text: 'Developper', 
+        sub: [
+            { text:'Web', imagePath:"/images/web.svg" },
+            { text:'Mobile', imagePath:"/images/mobile.svg" },
+        ] 
+    },
+    { text: 'Backend Developer', imagePath:"/images/api.svg" },
+];
---
+++ src/sections/Hero.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { roles } from './Hero'
+
+const Hero = () => {
+  return (
+    <section id="hero" className='flex flex-row relative overflow-hidden gap-5'>
+        <header className='hero-text'>
+            <h1>Alfred</h1> 
+            <h1>TCHOPPE YONA</h1>
+            <div>
+
+            </div>
+        </header>
+    </section>
+  )
+}
+
+export default Hero
